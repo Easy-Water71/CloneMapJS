bab9e9a0c49e19ea470dc742a0443ba9ca3f1505
Easy-Water71 <phllo154@naver.com>
2020-04-08 13:51:31 +0900

    initial commit

--- clock.js
+++ clock.js
@@ -1,12 +1,12 @@
 const clockContainer = document.querySelector(".clock");
-const clockTitle = clockContainer.querySelector("h1");
+const clock = clockContainer.querySelector("h1");
 
 function loadTime(){
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  clock.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init(){
